3d15b6944b0ab0280741e802e75bef2fd0989e7b
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-06-13 12:48:49 +0900

    commit

--- docs/test.js
+++ docs/test.js
@@ -11,7 +11,7 @@ btnMinMaxNumValue.onclick = evt => {
     gUM(minmaxConstraints.textContent);
 }
 function gUM(constraints) {
-    msg.innerHTML = constraints.replace(/\n/g, '<br>');
+    constraintsPreview.innerHTML = constraints.replace(/\n/g, '<br>');
     var constraintsObj = JSON.parse(constraints);
     preview.onloadedmetadata = evt => {
         msg.textContent = preview.videoWidth + ' x ' + preview.videoHeight;
