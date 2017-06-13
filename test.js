a767918df0505a3b7179606812d46e7d71d6b874
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-06-13 12:42:24 +0900

    commit

---
+++ test.js
@@ -0,0 +1,21 @@
+btnTrueFalse.onclick = evt => {
+    gUM(truefalseConstraints.textContent);
+}
+btnNumValue.onclick = evt => {
+    gUM(numValueConstraints.textContent);
+}
+btnExactNumValue.onclick = evt => {
+    gUM(exactConstraints.textContent);
+}
+btnMinMaxNumValue.onclick = evt => {
+    gUM(minmaxConstraints.textContent);
+}
+function gUM(constraints) {
+    var constraintsObj = JSON.parse(constraints);
+    preview.onloadedmetadata = evt => {
+        msg.textContent = preview.videoWidth + ' x ' + preview.videoHeight;
+    }
+    navigator.mediaDevices.getUserMedia(constraintsObj).then(stream => {
+        preview.srcObject = stream;
+    });
+}
