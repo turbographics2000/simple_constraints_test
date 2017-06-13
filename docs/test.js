32176f45835324bde3c1013e3a0a3e814dbcf53c
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-06-13 12:58:57 +0900

    commit

--- docs/test.js
+++ docs/test.js
@@ -11,6 +11,12 @@ btnMinMaxNumValue.onclick = evt => {
     gUM(minmaxConstraints.textContent);
 }
 function gUM(constraints) {
+    if(preview.srcObject) {
+        preview.srcObject.getTracks().forEach(track => {
+            track.stop();
+        });
+        preview.srcObject = null;
+    }
     constraintsPreview.innerHTML = constraints;
     var constraintsObj = JSON.parse(constraints);
     preview.onloadedmetadata = evt => {
@@ -18,5 +24,5 @@ function gUM(constraints) {
     }
     navigator.mediaDevices.getUserMedia(constraintsObj).then(stream => {
         preview.srcObject = stream;
-    });
+    }).catch(ex => msg.textContent = ex.message);
 }
